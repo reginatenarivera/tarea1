918bb978241c63ca525a49f9377d7c881746e530
reginatenarivera <53837239+reginatenarivera@users.noreply.github.com>
2019-08-19 17:18:08 -0500

    Add files via upload

---
+++ Problema6.cpp.cpp
@@ -0,0 +1,50 @@
+
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+
+{
+
+    float a;
+ 
+   float b;
+
+    float c;
+
+    float d; 
+
+    float e;
+
+    float promedio;
+ 
+  cout<<"introduce primer numero"<<endl;
+
+    cin>> a;
+
+  cout<<"introduce segundo numero"<<endl;
+
+    cin>> b;
+
+  cout<<"introduce tercer numero"<<endl;
+
+    cin>> c;
+
+  cout<<"introduce cuarto numero"<<endl;
+ 
+    cin>> d;
+
+  cout<<"intriduce quinto numero"<<endl;
+
+    cin>> e;
+
+    
+    promedio=((a +b +c +d +e)/5);
+
+  cout<<"El promedio es"<<promedio  <<endl;
+  
+return 0;
+}
